efefb4b4483fec3a4c9659f80a1692a20b5d2849
Cristobal Cuevas <kriistobal1@gmail.com>
2026-03-26 00:52:32 -0300

    change hour range from charts

--- src/views/PlantasView.jsx
+++ src/views/PlantasView.jsx
@@ -145,7 +145,7 @@ export const UbidotsView = () => {
 
       {/* Gráficas */}
       <h3 className="text-2xl font-semibold text-slate-700 mb-4 mt-8 border-b-2 border-slate-300 pb-2">
-        Gráficas históricas (Últimas 24h disponibles)
+        Gráficas históricas (Últimas 72h disponibles)
       </h3>
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
         {selectedPlant.sensors?.flatMap(sensor =>
